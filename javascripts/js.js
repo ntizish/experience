c815790bd67f4dc290cc2992c3f604b64ce1aa6d
ntizish <66680150+ntizish@users.noreply.github.com>
2022-03-11 20:58:05 +0300

    UPD First animations

--- javascripts/js.js
+++ javascripts/js.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     $("#chamelionImg").css("animation-play-state", "running");
     setTimeout(function() {
       $("#chamelionImg").css("animation-play-state", "paused");
-    }, 1800);
+    }, 2000);
   });
 });
 
@@ -18,3 +18,20 @@ $(document).ready(function(){
     $("#dayImg").css("display", "flex");
   });
 });
+
+$(document).ready(function(){
+  $("#sunEyes").click(function(){
+    $("#sunEyes").css("animation-play-state", "running");
+    $("#sunEyesTextImg").css("display", "flex");
+    setTimeout(function() {
+      $("#sunEyes").css("animation-play-state", "paused");
+    }, 3000);
+
+  });
+});
+
+$(document).ready(function(){
+  $("#sunEyesTextImg").click(function(){
+    $("#sunEyesTextImg").css("display", "none");
+  })
+})
